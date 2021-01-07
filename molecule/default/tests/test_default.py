a58e5b2ec1e1975832c340073ea3e740dae44e81
Bodo Schulz <bodo@boone-schulz.de>
2021-01-07 10:11:58 +0100

    add molecule tests
    rewrite jinja template for logrote configs to use more than one rotate block in one config file
    
    add disable_systemd part for systemd-less OS like devuan or artixlinux

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,50 @@
+import pytest
+import os
+import yaml
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.fixture()
+def get_vars(host):
+    defaults_files = "file=../../defaults/main.yml name=role_defaults"
+    vars_files = "file=../../vars/main.yml name=role_vars"
+
+    ansible_vars = host.ansible(
+        "include_vars",
+        defaults_files)["ansible_facts"]["role_defaults"]
+
+    ansible_vars.update(host.ansible(
+        "include_vars",
+        vars_files)["ansible_facts"]["role_vars"])
+
+    return ansible_vars
+
+
+@pytest.mark.parametrize("dirs", [
+    "/etc/logrotate.d",
+])
+def test_directories(host, dirs):
+    d = host.file(dirs)
+    assert d.is_directory
+    assert d.exists
+
+
+@pytest.mark.parametrize("files", [
+    "/etc/logrotate.conf",
+    "/etc/logrotate.d/nofunc",
+    "/etc/logrotate.d/audit",
+    "/etc/logrotate.d/system",
+    "/etc/logrotate.d/icinga2",
+])
+def test_files(host, files):
+    f = host.file(files)
+    assert f.exists
+    assert f.is_file
+
+
+def test_service(host):
+    service = host.service("logrotate.timer")
+    assert not service.is_enabled
